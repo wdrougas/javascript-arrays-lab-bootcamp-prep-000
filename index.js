5dee7b7d47f0d42869925b265c65b2298ab7da88
Will Drougas <wdrougas@gmail.com>
2019-08-13 17:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(array) {
+function destructivelyAppendKitten(name) {
   return kittens.push("Ralph")
 }
 
-function destructivelyPrependKitten(array) {
+function destructivelyPrependKitten(name) {
   return kittens.unshift("Bob")
 }
 
-function destructivelyRemoveLastKitten(array) {
+function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten(array) {
+function destructivelyRemoveFirstKitten() {
   return kittens.shift()
 }
 
-function appendKitten(array) {
-  return [...array, "Broom"];
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten (name) {
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1,3)
 }
